5691588760bc20a2368e8275a860289e171e6032
joinmouse <joinmouse@foxmail.com>
2025-11-14 17:55:15 +0800

    feat: 完善DebtToken

--- pledge-contract/contracts/pledge/DebtToken.sol
+++ pledge-contract/contracts/pledge/DebtToken.sol
@@ -6,46 +6,24 @@ import "./AddressPrivileges.sol";
 
 /**
  * @title DebtToken
- * @dev 债务代币合约 - 用于表示用户在借贷协议中的债务
- * 只有具有 Minter 权限的地址（如借贷池合约）才能铸造和销毁代币
+ * @dev 债务代币合约，只有Minter可以铸造和销毁
  */
 contract DebtToken is ERC20, AddressPrivileges {
     
-    // ============ 构造函数 ============
-    
-    /**
-     * @notice 构造函数
-     * @param _name 代币名称
-     * @param _symbol 代币符号
-     * @param multiSignature 多签钱包地址
-     */
+    /// @notice 构造函数
     constructor(
         string memory _name, 
         string memory _symbol, 
         address multiSignature
     ) ERC20(_name, _symbol) AddressPrivileges(multiSignature) {}
     
-    // ============ 外部函数 ============
-    
-    /**
-     * @notice 铸造代币
-     * @dev 只有 Minter 可以调用
-     * @param _to 接收地址
-     * @param _amount 铸造数量
-     * @return 是否成功
-     */
+    /// @notice 铸造代币，只有Minter可调用
     function mint(address _to, uint256 _amount) external onlyMinter returns (bool) {
         _mint(_to, _amount);
         return true;
     }
     
-    /**
-     * @notice 销毁代币
-     * @dev 只有 Minter 可以调用
-     * @param _from 销毁地址
-     * @param _amount 销毁数量
-     * @return 是否成功
-     */
+    /// @notice 销毁代币，只有Minter可调用
     function burn(address _from, uint256 _amount) external onlyMinter returns (bool) {
         _burn(_from, _amount);
         return true;
